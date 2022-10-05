8cec7f467ccce7eaf407f8a9ec2d12dee2518283
Thibaut Soudan <tsoudan@hopper.com>
2022-10-05 10:48:42 +0200

    Replaced type List with Map in CFAR offer description

--- src/main/java/com/hopper/cloud/airlines/model/CfarOffer.java
+++ src/main/java/com/hopper/cloud/airlines/model/CfarOffer.java
@@ -86,7 +86,7 @@ public class CfarOffer {
 
   public static final String SERIALIZED_NAME_OFFER_DESCRIPTION = "offer_description";
   @SerializedName(SERIALIZED_NAME_OFFER_DESCRIPTION)
-  private List<String> offerDescription = new ArrayList<>();
+  private Map<String, List<String>> offerDescription = new HashMap<>();
 
   public static final String SERIALIZED_NAME_EXT_ATTRIBUTES = "ext_attributes";
   @SerializedName(SERIALIZED_NAME_EXT_ATTRIBUTES)
@@ -302,14 +302,14 @@ public class CfarOffer {
   }
 
 
-  public CfarOffer offerDescription(List<String> offerDescription) {
+  public CfarOffer offerDescription(Map<String, List<String>> offerDescription) {
     
     this.offerDescription = offerDescription;
     return this;
   }
 
-  public CfarOffer addOfferDescriptionItem(String offerDescriptionItem) {
-    this.offerDescription.add(offerDescriptionItem);
+  public CfarOffer putOfferDescriptionItem(String key, List<String> offerDescriptionItem) {
+    this.offerDescription.put(key, offerDescriptionItem);
     return this;
   }
 
@@ -320,12 +320,12 @@ public class CfarOffer {
   @javax.annotation.Nonnull
   @ApiModelProperty(required = true, value = "")
 
-  public List<String> getOfferDescription() {
+  public Map<String, List<String>> getOfferDescription() {
     return offerDescription;
   }
 
 
-  public void setOfferDescription(List<String> offerDescription) {
+  public void setOfferDescription(Map<String, List<String>> offerDescription) {
     this.offerDescription = offerDescription;
   }
 
