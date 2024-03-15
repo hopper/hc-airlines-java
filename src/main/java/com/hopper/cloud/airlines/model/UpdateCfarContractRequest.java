725554aa724afc5680ca47d29a72fa2fcb46dc90
Olivier Gasparini <ogasparini@hopper.com>
2024-03-15 11:40:15 +0100

    HCAP-654 Remove useless import

--- src/main/java/com/hopper/cloud/airlines/model/UpdateCfarContractRequest.java
+++ src/main/java/com/hopper/cloud/airlines/model/UpdateCfarContractRequest.java
@@ -20,7 +20,6 @@ import com.google.gson.*;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import com.hopper.cloud.airlines.model.CfarStatus;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.io.IOException;
